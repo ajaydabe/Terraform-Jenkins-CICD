f2c2ea186b5ac1a57cfb59a4e83dfc0b5072a127
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-26 19:10:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "new-south"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,12 +12,12 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-09b0a86a2c84101e1"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "terraform-jenkins-cicd-bucket"
 }
 
 variable "acl" {
